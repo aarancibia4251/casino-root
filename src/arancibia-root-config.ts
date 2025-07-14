0b1a3310943ef6a40f637ffec9b681dfcb5627be
aarancis <alexis2396@hotmail.com>
2025-07-14 09:30:08 +0900

    all-projects - feature: organized import maps - feature: added link for every project

--- src/arancibia-root-config.ts
+++ src/arancibia-root-config.ts
@@ -1,9 +1,22 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
+(window as any).global = window;
 
 registerApplication({
   name: "@arancibia/casino-client",
   app: () => System.import<LifeCycles>("@arancibia/casino-client"),
-  activeWhen: ["/"],
+  activeWhen: ["/casino-project"],
+});
+
+registerApplication({
+  name: "@arancibia/appsteps",
+  app: () => System.import<LifeCycles>("@arancibia/appsteps"),
+  activeWhen: ["/pwa-project"],
+});
+
+registerApplication({
+  name: "@arancibia/bodega",
+  app: () => System.import<LifeCycles>("@arancibia/bodega"),
+  activeWhen: ["/bodega-project"],
 });
 
 start({
